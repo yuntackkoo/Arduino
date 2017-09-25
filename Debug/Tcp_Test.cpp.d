31a97e656459a69311e3d8279f09aa06f26769aa
yuntackkoo <flyiceball@gmail.com>
2017-09-26 08:27:26 +0900

    .
    .

--- Debug/Tcp_Test.cpp.d
+++ Debug/Tcp_Test.cpp.d
@@ -1,5 +1,5 @@
-C:\Users\Koo\AppData\Local\Temp\VMBuilds\Tcp_Test\yun\Debug\Tcp_Test.cpp.o: \
- C:\Users\Koo\AppData\Local\Temp\VMBuilds\Tcp_Test\yun\Debug\Tcp_Test.cpp \
+C:\Users\Koo\AppData\Local\Temp\VMBuilds\Tcp_Test\leonardo\Debug\Tcp_Test.cpp.o: \
+ C:\Users\Koo\AppData\Local\Temp\VMBuilds\Tcp_Test\leonardo\Debug\Tcp_Test.cpp \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
@@ -12,19 +12,21 @@ C:\Users\Koo\AppData\Local\Temp\VMBuilds\Tcp_Test\yun\Debug\Tcp_Test.cpp.o: \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\yun/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\yun/../leonardo/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/Bridge.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\leonardo/pins_arduino.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/Adafruit_CC3000.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/Console.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/Process.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/YunServer.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/BridgeServer.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Server.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/YunClient.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Bridge\src/BridgeClient.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/cc3000_common.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/data_types.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/error_codes.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/wlan.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/cc3000_common.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/utility/netapp.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/ccspi.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Client.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/Servo.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/avr/ServoTimers.h
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/Adafruit_CC3000_Server.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/Adafruit_CC3000.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Server.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/Adafruit_CC3000_Server.h \
+ C:\Users\Koo\Documents\Arduino\libraries\Adafruit_CC3000_Library-master/ccspi.h
--- Tcp_Test.ino
+++ Tcp_Test.ino
@@ -1,13 +1,15 @@
-#include <Bridge.h>
-#include <YunServer.h>
-#include <YunClient.h>
-#include <time.h>
-#include <Servo.h>
+#include <Adafruit_CC3000.h>
+#include <Adafruit_CC3000_Server.h>
+#include <ccspi.h>
+#include <SPI.h>
 
+#define ADAFRUIT_CC3000_IRQ 7 //인터럽트 컨트롤 핀, Wido 보드 사용시 7로 변경.
+#define ADAFRUIT_CC3000_VBAT 5
+#define ADAFRUIT_CC3000_CS 10
+#define MAX_LENGTH 33
 #define PORT 255
 #define TOTALL_PACK 33    //총 패킷 사이즈 33Byte
-#define MAX_CLIENT 5
-#define MAX_USER 12
+
 
 const byte Re_Request = 0;//재요청
 const byte Join = 1;//접속
@@ -24,13 +26,12 @@ const byte Offer_Data = 63;//로그 요청 받을시 로그 응답
 const byte Unlock_Other = 64;//다른 사람이 문 열때 서버에서 전송
 
 
-YunServer server(PORT);
-YunClient client[MAX_CLIENT];
-long seq_num_arr[MAX_CLIENT];
-String usertable[MAX_USER];
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
 
-static boolean clientActive = false;
-char SerialChat;
+String SSID;
+String PASS;
+
+#define WLAN_SECURITY WLAN_SEC_WPA2
 
 typedef struct _packet {
 	byte datasize;
@@ -45,9 +46,142 @@ byte packetBuffer[TOTALL_PACK];
 byte sendBuffer[TOTALL_PACK];
 Packet recpacket;
 Packet sendpacket;
-Servo servo;
-int Angle;
-unsigned int cuid = 0;
+
+
+
+
+
+
+void setup() {
+	Serial.begin(9600);
+
+	SSID = String("iptimer");
+	PASS = String("flyiceball!");
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	//Wi-Fi 연결 시도
+	if (!cc3000.begin())
+	{
+		Serial.println(F("Couldn't begin()! Check your wiring?"));
+		while (1);
+	}
+
+	wifiScan();
+
+	joinWiFi();
+}
+
+void loop() {
+	if (cc3000.getStatus() != STATUS_CONNECTED) {
+		Serial.println();
+		Serial.println("WiFi Connect Lost ReConnect!");
+		Serial.println();
+		
+		digitalWrite(LED_BUILTIN, LOW);
+
+		cc3000.reboot();
+
+		joinWiFi();
+	}
+	delay(1000);
+}
+
+bool displayConnectionDetails(void)
+{
+	uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+
+	if (!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+	{
+		Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+		return false;
+	}
+	else
+	{
+		Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+		Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+		Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+		Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+		Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+		Serial.println();
+		return true;
+	}
+}
+
+bool joinWiFi(void) {
+	Serial.print(F("\nAttempting to connect to "));
+	Serial.println(SSID);
+	char *CSSID = (char *)malloc((SSID.length()) * sizeof(char));
+	char *CPASS = (char *)malloc((PASS.length()) * sizeof(char));
+
+	SSID.toCharArray(CSSID, SSID.length()+1);
+	PASS.toCharArray(CPASS, PASS.length()+1);
+
+	if (!cc3000.connectToAP(CSSID, CPASS, WLAN_SECURITY)) {
+		Serial.println(F("Failed!"));
+		return false;
+	}
+
+	Serial.println(F("Connected!"));
+
+	Serial.println(F("Request DHCP"));
+	while (!cc3000.checkDHCP())
+	{
+		delay(100); // ToDo: Insert a DHCP timeout!
+	}
+
+	while (!displayConnectionDetails()) {
+		delay(1000);
+	}
+
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	free(CSSID);
+	free(CPASS);
+
+	return true;
+}
+
+void wifiScan(void)
+{
+	uint8_t valid, rssi, sec;
+	uint32_t index;
+	uint32_t index2;
+	char ssidname[33];
+	cc3000.startSSIDscan(&index);
+	Serial.print(("Networks found : "));
+	Serial.println(index);
+	Serial.println((" == == == == == == == == == == == == == == == == == == == == == == == == "));
+	while (index) {
+		index--;
+		valid = cc3000.getNextSSID(&rssi, &sec, ssidname);
+		Serial.print(("SSID Name : "));
+		Serial.print(ssidname);
+		Serial.println();
+		Serial.print(("RSSI : "));
+		Serial.println(rssi);
+		Serial.print(("Security Mode : "));
+		Serial.println(sec);
+		Serial.println();
+	}
+	Serial.println((" == == == == == == == == == == == == == == == == == == == == == == == == "));
+	cc3000.stopSSIDscan();
+}
+
+void FillSendBuffer(int datasize) {
+	int offset = 0;
+	sendBuffer[offset++] = sendpacket.code;
+	sendpacket.paddingSize = TOTALL_PACK - datasize - 7;
+	sendpacket.datasize = 26 - sendpacket.paddingSize;
+	sendBuffer[offset++] = sendpacket.paddingSize;
+	for (; offset < sendpacket.paddingSize + 2; offset++) {
+		sendBuffer[offset] = rand();
+	}
+	offset++;
+	byte *p = (byte *)&sendpacket.seqNum;
+	for (int i = 0; i < datasize + 5; i++) {
+		sendBuffer[offset++] = *p++;
+	}
+}
 
 void Parsing(byte *packet) {
 	int offset = 0;
@@ -57,117 +191,97 @@ void Parsing(byte *packet) {
 	offset += recpacket.paddingSize; //패딩부분을 지나침
 	pointer += recpacket.paddingSize; //패딩부분을 지나침
 	recpacket.datasize = TOTALL_PACK - 7 - recpacket.paddingSize;
-	for (int i = offset; i<offset+4; i++) {
+	for (int i = offset; i<offset + 4; i++) {
 		*pointer++ = packet[i];//패딩부분 이후 나머지 데이터를 복사
 	}
+
 	offset += 5;
+
 	for (int i = 0; i < recpacket.datasize; i++) {
 		recpacket.data[i] = packet[offset++];
 	}
 }
 
-void FillSendBuffer(int datasize) {
-	int offset = 0;
-	sendBuffer[offset++] = sendpacket.code;
-	sendpacket.paddingSize = TOTALL_PACK - datasize - 7;
-	sendpacket.datasize = 26 - sendpacket.paddingSize;
-	sendBuffer[offset++] = sendpacket.paddingSize;
-	for (; offset < sendpacket.paddingSize+2; offset++) {
-		sendBuffer[offset] = rand();
-	}
-	offset++;
-	byte *p = (byte *)&sendpacket.seqNum;
-	for (int i = 0; i < datasize + 5; i++) {
-		sendBuffer[offset++] = *p++;
-	}
-	
-}
-
-void SwitchPacket(int number);
-
-void setup() {
-	Serial.begin(9600);
-	Bridge.begin();
-	server.noListenOnLocalhost();
-	server.begin();
-	srand((unsigned) time(NULL));
-	randomSeed((unsigned) time(NULL));
-}
-
-int to_int(byte b) {
-	return (b & 0xFFFF);
-}
-
-void initPacket() {
-	recpacket.code = -1;
-}
-
-void loop() {
-	int i = 0;
-	for (i = 0; i < MAX_CLIENT; i++) {
-		if (!client[i].connected()) {
-			client[i] = server.accept();
-			
-		}
-	}
-
-	for (i = 0; i < MAX_CLIENT; i++) {
-		if (client[i].connected()) {
-			SwitchPacket(i);
-		}
-	}
-}
-
 void SwitchPacket(int number) {
+
 	if (client[number].connected()) {
+
 		int i = 0;
+
 		if (!client[number].connected()) {
+
 			Serial.println("New client connection.");
+
 		}
+
+
 
 		if (client[number].available()) {
-			client[number].readBytes(packetBuffer, TOTALL_PACK);
-			Parsing(packetBuffer);
-			int _code = to_int(recpacket.code);
-			int _id = to_int(recpacket.id);
-			
-			Serial.print("Code : ");
-			Serial.println(_code);
-			Serial.print("Padding Size : ");
-			Serial.println(recpacket.paddingSize);
-			Serial.print("SeqNum : ");
-			Serial.println(recpacket.seqNum, DEC);
-			Serial.print("ID : ");
-			Serial.println(_id);
-			
-		}
 
-		if (Serial.available()) {
-			SerialChat = Serial.read();
-			Serial.print(SerialChat);
-			client[number].print(SerialChat);
+			client[number].readBytes(packetBuffer, TOTALL_PACK);
+
+			Parsing(packetBuffer);
+
+			int _code = to_int(recpacket.code);
+
+			int _id = to_int(recpacket.id);
+			Serial.print("Code : ");
+
+			Serial.println(_code);
+
+			Serial.print("Padding Size : ");
+
+			Serial.println(recpacket.paddingSize);
+
+			Serial.print("SeqNum : ");
+
+			Serial.println(recpacket.seqNum, DEC);
+
+			Serial.print("ID : ");
+
+			Serial.println(_id);
 		}
 
 		switch (recpacket.code)
+
 		{
+
 		case Join:
+
 			Serial.println("\n\nStart Send Packet\n");
+
 			sendpacket.code = Response;
+
 			if (recpacket.id < 0) {
+
 				sendpacket.id = recpacket.id;
+
 			}
+
 			else
+
 			{
+
 				sendpacket.id = cuid++;
+
 			}
+
 			sendpacket.seqNum = random();
+
 			seq_num_arr[number] = sendpacket.seqNum;
+
 			FillSendBuffer(0);
+
 			client[number].write(sendBuffer, TOTALL_PACK);
+
 			Serial.println("Complete Send Packet");
+
 			Serial.print("Send Seq_Num : ");
+
 			Serial.println(sendpacket.seqNum);
+
 			break;
+
 		case Key_Ex:
 			Serial.println("\n\nStart Send Packet\n");
 			usertable[recpacket.id] = String((char *)recpacket.data);
@@ -178,6 +292,7 @@ void SwitchPacket(int number) {
 				Serial.print((char)recpacket.data[i]);
 			}
 			break;
+
 		case UnLock:
 			Serial.println("\n\nStart Send Packet\n");
 			sendpacket.code = Unlock_Other;
@@ -191,17 +306,25 @@ void SwitchPacket(int number) {
 				if (client[i].connected()) {
 					if (i != number) {
 						sendpacket.seqNum = seq_num_arr[i];
-						client[i].write(sendBuffer,TOTALL_PACK);
+						client[i].write(sendBuffer, TOTALL_PACK);
 					}
 				}
 			}
-			digitalWrite(13, HIGH);
-			delay(1000);
-			digitalWrite(13, LOW);
 			break;
 		default:
 			break;
 		}
 		initPacket();
 	}
+}
+
+
+
+int to_int(byte b) {
+	return (b & 0xFFFF);
+}
+
+void initPacket() {
+	recpacket.code = -1;
+
 }
